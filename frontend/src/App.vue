cd86145b8906c394c4de18ef14a7eda12c5a0153
Pomares Jonathan <pomares.jonathan@gmail.com>
2022-03-28 12:30:48 +0200

    header

---
+++ frontend/src/App.vue
@@ -0,0 +1,20 @@
+<!--------------------------------HTML-------------------------------->
+<template>
+
+  <headerLink />
+
+</template>
+
+<!--------------------------------JAVASCRIPT-------------------------------->
+<script>
+
+  import headerLink from "./components/header.vue"
+  export default {
+    name: "headerLink",
+    components: {
+      headerLink
+    }
+    
+}
+
+</script>
